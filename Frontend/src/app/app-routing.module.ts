ae0e8708096d08cafe1f2d0dcf5fa26fa292e814
Pasan-Wijerathne <pasanheshan@gmail.com>
2018-04-08 11:35:02 +0530

    create the home page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { BillmanagerComponent } from './billmanager/billmanager.component';
 
 const routes: Routes = [
   {path : "home", component : HomeComponent},
   {path : "login", component : LoginComponent},
+  {path : "billmanager", component : BillmanagerComponent},
+  
   ];
 
+
+  
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
